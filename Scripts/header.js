5d8e9c6435318f4409f66353af6bcb906ba55480
ChpoCko <t2gaiu00@students.oamk.fi>
2023-03-25 17:23:06 +0200

    Menu popup animations added

--- Scripts/header.js
+++ Scripts/header.js
@@ -7,8 +7,9 @@ const accountBtn = document.querySelector(`.account`);
 const menuBtn = document.querySelector(`.menu-icon`);
 const menuPopup = document.querySelector(`.header__menu-popup`);
 const menuItemBtns = document.querySelectorAll(`.header__menu--item`);
+const menuPageLink = document.querySelectorAll(`.menu-page-link`);
 
-//Function on account button click
+//Function on account icon button click
 const accClick = () => {
   accountBtn.addEventListener('click', () => {
     // if signup and login contain hidden class > remove it and start animation to left
@@ -25,8 +26,8 @@ const accClick = () => {
       // start animation to right
       signUpBtn.classList.remove(`toLeftL`);
       logInBtn.classList.remove(`toLeftS`);
-      logInBtn.style.animation = `toRightS 0.8s ease-out`;
-      signUpBtn.style.animation = `toRightL 0.8s ease-out`;
+      logInBtn.style.animation = `toRightS 0.8s ease-in`;
+      signUpBtn.style.animation = `toRightL 0.8s ease-in`;
       // After animation run (a little bit earlier), remove any animation style and add hidden class
       setTimeout(() => {
         logInBtn.style.animation = ``;
@@ -39,36 +40,69 @@ const accClick = () => {
 };
 accClick(); //Call this function if user is NOT logged in
 
-menuBtn.addEventListener('click', () => {
-  if (menuPopup.classList.contains(`no-display`)) {
-    menuPopup.classList.remove(`no-display`);
-    // menuPopup.classList.add(`menu-open`);
-
-    // menuItemBtns.classList.add(`menuItemToRight`);
+// This function to hide a menu
+const hideMenu = () => {
+  console.log(`Menu hidden`);
+  menuPopup.style.animation = `menuReduce 0.8s ease-in`;
+  menuPopup.classList.remove(`menuExpand`);
+  menuItemBtns.forEach(menuItem => {
+    menuItem.classList.remove(`menuItemToRight`);
+  });
+  menuItemBtns.forEach(menuItem => {
+    menuItem.style.animation = 'menuItemToLeft 0.8s ease-in';
+  });
+  setTimeout(() => {
     menuItemBtns.forEach(menuItem => {
-      menuItem.classList.add(`menuItemToRight`);
+      menuItem.style.animation = ``;
     });
+    menuPopup.style.animation = ``;
+    menuPopup.classList.add(`no-display`);
+  }, 790);
+};
+
+const openMenu = () => {
+  menuPopup.classList.remove(`no-display`);
+  menuPopup.classList.add(`menuExpand`);
+
+  menuItemBtns.forEach(menuItem => {
+    menuItem.classList.add(`menuItemToRight`);
+  });
+  console.log(`Menu opened`);
+};
+
+window.onclick = function (event) {
+  if (event.target.matches(`.menu-icon`)) {
+    if (menuPopup.classList.contains(`no-display`)) {
+      openMenu();
+    } else {
+      hideMenu();
+    }
   } else {
-    // menuItemBtns.style.animation = 'menuItemToLeft 0.8s ease-in';
-    menuItemBtns.forEach(menuItem => {
-      menuItem.classList.remove(`menuItemToRight`);
-    });
-    // for (let i = 0; i < menuItemBtns.length; i++) {
-    //   menuItemBtns[i].style.animationDelay = `0.${i}s`;
-    //   // console.log(menuItemBtns[i]);
-    // }
-
-    menuItemBtns.forEach(menuItem => {
-      menuItem.style.animation = 'menuItemToLeft 0.8s ease-in';
-    });
-
-    setTimeout(() => {
-      // menuItemBtns.style.animation = ``;
-      menuItemBtns.forEach(menuItem => {
-        menuItem.style.animation = ``;
-      });
-      menuPopup.classList.add(`no-display`);
-      // menuPopup.classList.remove(`menu-open`);
-    }, 790);
+    hideMenu();
   }
-});
+};
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (
+    prevScrollpos !== currentScrollPos &&
+    !menuPopup.classList.contains(`no-display`)
+  ) {
+    hideMenu();
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+// class Header {
+//   accBtn;
+//   menuBtn2;
+//   constructor(accBtn, menuButton) {
+//     this.accBtn = accBtn;
+//     this.menuBtn2 = menuButton;
+//   }
+// }
+
+// const header = new Header(accountBtn, menuBtn);
+
+// console.log(header.accBtn);
