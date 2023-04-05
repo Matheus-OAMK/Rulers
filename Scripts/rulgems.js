d4614fcc45489c16dcb0decd0a25eb59c8445d87
Hakan Asmaoglu <114767811+Hakan-Asmaoglu@users.noreply.github.com>
2023-04-05 14:46:36 +0300

    function unavailable added

--- Scripts/rulgems.js
+++ Scripts/rulgems.js
@@ -1,1 +1,27 @@
-"use strict";
+//Get info modal
+
+const infoGemsModal = document.querySelector('.gems-modal');
+const infoGemsOverlay = document.querySelector('.gems-overlay');
+const infoGemsCloseModalbtn = document.querySelector('.gems-info-close-modal');
+
+//Get info open 
+const infoGemsOpenModalbtn = document.querySelectorAll('.gems-show-modal');
+
+console.log(infoGemsOpenModalbtn);
+
+for (let i = 0; i < infoGemsOpenModalbtn.length; i++) {
+  infoGemsOpenModalbtn[i].addEventListener('click', function () {
+    console.log('clicked');
+    infoGemsModal.classList.remove('gems-hidden'); // <-- fixed typo here
+    infoGemsOverlay.classList.remove('gems-hidden');
+  });
+}
+
+console.log(infoGemsCloseModalbtn);
+
+// Add event listener to close modal button
+infoGemsCloseModalbtn.addEventListener('click', () => {
+  console.log('clicked close');
+  infoGemsModal.classList.add('gems-hidden');
+  infoGemsOverlay.classList.add('gems-hidden');
+});
