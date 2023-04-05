e54a49a3ba5b50e73e33f05767b0633449b78d75
ChpoCko <t2gaiu00@students.oamk.fi>
2023-04-05 19:35:12 +0300

    Custom checkboxes added

--- Scripts/allcards.js
+++ Scripts/allcards.js
@@ -3,3 +3,17 @@
 // const option = document.querySelector('.option');
 
 // console.log(option.value);
+
+// const new1 = document.querySelectorAll(`.filtering-option--input`);
+
+// new1.forEach(element => {
+//   element.addEventListener('input', () => {
+//     console.log(element.name);
+//   });
+// });
+
+// const slider = document.querySelector(`.filter-slider`);
+
+// slider.addEventListener('input', () => {
+//   console.log(slider.value);
+// });
