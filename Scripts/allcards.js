3f87cc5dcc64dd09316d59060157313849a969c3
ChpoCko <t2gaiu00@students.oamk.fi>
2023-04-06 19:08:19 +0300

    Filter expands

--- Scripts/allcards.js
+++ Scripts/allcards.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// const option = document.querySelector('.option');
-
-// console.log(option.value);
-
 // const new1 = document.querySelectorAll(`.filtering-option--input`);
 
 // new1.forEach(element => {
@@ -12,8 +8,35 @@
 //   });
 // });
 
-// const slider = document.querySelector(`.filter-slider`);
+const slider = document.querySelector(`.filter-slider`);
+const sytleTag = document.querySelector(`.style-for-slider`);
+const filterBtn = document.querySelector(`.filter-btn`);
+const filterOptionsBox = document.querySelector(`.filtering-options-box`);
+const filterInnerSectionBox = document.querySelectorAll(
+  '.filter-inner-section'
+);
 
-// slider.addEventListener('input', () => {
-//   console.log(slider.value);
-// });
+filterBtn.addEventListener('click', () => {
+  if (filterOptionsBox.classList.contains('filter-options-hidden')) {
+    filterOptionsBox.classList.remove('filter-options-hidden');
+  } else {
+    filterOptionsBox.style.animation = 'filterReduce 0.6s ease-in';
+
+    filterInnerSectionBox.forEach(innerBox => {
+      innerBox.style.animation = 'filterInnerSectionOut 0.2s ease-in';
+    });
+
+    setTimeout(() => {
+      filterOptionsBox.style.animation = '';
+      filterInnerSectionBox.forEach(innerBox => {
+        innerBox.style.animation = '';
+      });
+
+      filterOptionsBox.classList.add('filter-options-hidden');
+    }, 590);
+  }
+});
+
+slider.addEventListener('input', () => {
+  sytleTag.innerHTML = `.filter-slider::after {content: '${slider.value}';z-index: 3;heigth: 6px;}`;
+});
