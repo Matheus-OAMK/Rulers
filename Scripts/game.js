28e8dff9e7a1b51f1a5b447270d0f5a22678c3e4
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-04-07 10:37:01 +0300

    an updated version of our rulers game

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,42 +1,66 @@
-"use strict";
-const cards = document.querySelectorAll(".game--card");
-const playBtn = document.querySelector(".game--btn");
-const card1Img = document.querySelector("#game--card-img-1");
-const card2Img = document.querySelector("#game--card-img-2");
-const card3Img = document.querySelector("#game--card-img-3");
-const card4Img = document.querySelector("#game--card-img-4");
+'use strict';
+const cards = document.querySelectorAll('.game--card');
+const playBtn = document.querySelector('.game--btn');
+const card1Img = document.querySelector('#game--card-img-1');
+const card2Img = document.querySelector('#game--card-img-2');
+const card3Img = document.querySelector('#game--card-img-3');
+
+const cardsImg = document.querySelectorAll('.game--card-inner');
+
+playBtn.addEventListener('click', () => {
+  //disable button so they cant click it again until game is over
+  playBtn.disabled = true;
+
+  //change card images that the users will win
+  card1Img.src = '/Images/Epic cards/FRONT/AHRI.png';
+  card2Img.src = '/Images/Rare cards/FRONT/GAREN.png';
+  card3Img.src = '/Images/Legendary cards/FRONT/AZIR.png';
 
 
-playBtn.addEventListener("click", () => {
-  playBtn.disabled = true;	
-  cards.forEach(card => {
-    card.style.left = '50%';
-    card.style.transform = 'translateX(-25%)'
-  }); 
+  setTimeout(() => {
+    cards[2].style.transform = `translate(8rem,-35rem) rotate(0deg)`;
+  }, 0);
+
+  setTimeout(() => {
+    cards[1].style.transform = `translate(0rem,-35rem) rotate(0deg)`;
+  }, 700);
 
 
-  //return cards to position  
+  setTimeout(() => {
+    cards[0].style.transform = `translate(-8rem,-35rem) rotate(0deg)`;
+  }, 1400);
+
+
+  // //move cards above
+  // cards[0].style.transform = `translate(-8rem,-35rem) rotate(0deg)`;
+  // cards[1].style.transform = `translate(0rem,-35rem) rotate(0deg)`;
+  // cards[2].style.transform = `translate(8rem,-35rem) rotate(0deg)`;
+
+//flip cards to reveal the images
+  setTimeout(() => {
+    cardsImg.forEach(card => {
+          card.classList.add('flipped');
+        });
+    }, 3000);
+
+    //return cards back to start position 
   setTimeout(() => {
     cards.forEach(card => {
-      card.style.left = '';
-      card.style.transform = ''
+      card.style.transform = '';
     });
-    //change card images
-    card1Img.src = '/Images/Epic cards/FRONT/AHRI.png';
-    card2Img.src = '/Images/Epic cards/FRONT/AHRI.png';
-    card3Img.src = '/Images/Epic cards/FRONT/AHRI.png';
-    card4Img.src = '/Images/Epic cards/FRONT/AHRI.png';
-    
-  }, 1500);
+  }, 6500);
 
   //reset the cards to question mark
   setTimeout(() => {
-    const cardsImg = document.querySelectorAll('[id^="game--card"]');
     cardsImg.forEach(card => {
-      card.src = '/Images/Game/RANDOM EPIC.png';});
-      playBtn.disabled = false;	
+      card.style.transform = '';
+      card.classList.remove('flipped');
+    });
   }, 6000);
- });
 
+//re enable button
+  setTimeout(() => {
+    playBtn.disabled = false;
+  }, 7500);
 
-
+});
