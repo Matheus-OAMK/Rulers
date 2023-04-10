abbc3a45bc15021e6e0a362f1ba460d5f941a333
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-04-10 17:44:05 +0300

    moved the title and changed animations slightly

--- Scripts/game.js
+++ Scripts/game.js
@@ -4,8 +4,8 @@ const playBtn = document.querySelector('.game--btn');
 const card1Img = document.querySelector('#game--card-img-1');
 const card2Img = document.querySelector('#game--card-img-2');
 const card3Img = document.querySelector('#game--card-img-3');
-
-const cardsImg = document.querySelectorAll('.game--card-inner');
+const cardsInner = document.querySelectorAll('.game--card-inner');
+const gameTitle = document.querySelector('.game--title');
 
 playBtn.addEventListener('click', () => {
   //disable button so they cant click it again until game is over
@@ -16,7 +16,10 @@ playBtn.addEventListener('click', () => {
   card2Img.src = '/Images/Rare cards/FRONT/GAREN.png';
   card3Img.src = '/Images/Legendary cards/FRONT/AZIR.png';
 
+  //Hide the title 
+  gameTitle.style.opacity = '0';
 
+//Move cards above and to the side at different times
   setTimeout(() => {
     cards[2].style.transform = `translate(8rem,-15vh) rotate(0deg)`;
   }, 0);
@@ -25,20 +28,14 @@ playBtn.addEventListener('click', () => {
     cards[1].style.transform = `translate(0rem,-15vh) rotate(0deg)`;
   }, 700);
 
-
   setTimeout(() => {
     cards[0].style.transform = `translate(-8rem,-15vh) rotate(0deg)`;
   }, 1400);
 
 
-  // //move cards above
-  // cards[0].style.transform = `translate(-8rem,-35rem) rotate(0deg)`;
-  // cards[1].style.transform = `translate(0rem,-35rem) rotate(0deg)`;
-  // cards[2].style.transform = `translate(8rem,-35rem) rotate(0deg)`;
-
 //flip cards to reveal the images
   setTimeout(() => {
-    cardsImg.forEach(card => {
+    cardsInner.forEach(card => {
           card.classList.add('flipped');
         });
     }, 3000);
@@ -52,14 +49,20 @@ playBtn.addEventListener('click', () => {
 
   //reset the cards to question mark
   setTimeout(() => {
-    cardsImg.forEach(card => {
+    cardsInner.forEach(card => {
       card.style.transform = '';
       card.classList.remove('flipped');
     });
   }, 6000);
 
+  setTimeout(() => {
+    gameTitle.style.opacity = '';
+  }, 7000);
+
+
 //re enable button
   setTimeout(() => {
+    gameTitle.style.opacity = '';
     playBtn.disabled = false;
   }, 7500);
 
