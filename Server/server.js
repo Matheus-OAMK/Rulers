0bc7265dba704206fe3d5282c4febc63a91d7d25
ChpoCko <t2gaiu00@students.oamk.fi>
2023-04-18 23:52:24 +0300

    SSL certificates configured

--- Server/server.js
+++ Server/server.js
@@ -31,14 +31,10 @@ exports.openDb = () => {
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
         ssl: {
-          rejectUnauthorized: true,
-          ca: fs
-            .readFileSync('/path/to/server-certificates/root.crt')
-            .toString(),
-          key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
-          cert: fs
-            .readFileSync('/path/to/client-certificates/postgresql.crt')
-            .toString(),
+          rejectUnauthorized: false,
+          ca: fs.readFileSync(`${__dirname}/DB_SSL/root.crt`).toString(),
+          key: fs.readFileSync(`${__dirname}/DB_SSL/server.key`).toString(),
+          cert: fs.readFileSync(`${__dirname}/DB_SSL/server.crt`).toString(),
         },
       });
   }
