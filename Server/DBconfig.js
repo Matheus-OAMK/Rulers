87e3c203bde621d4548a1c708bf56c4017a099c5
ChpoCko <t2gaiu00@students.oamk.fi>
2023-04-18 19:47:41 +0300

    file tree cleaned

--- Server/DBconfig.js
+++
@@ -1,8 +0,0 @@
-exports.DBconfig = {
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: true },
-};
