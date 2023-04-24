74641c8912e4782bad382d6cd09cd554d3524369
ChpoCko <t2gaiu00@students.oamk.fi>
2023-04-24 20:51:55 +0300

    Routes protection function, based on user cookies only

--- Server/Routes/cardRoutes.js
+++ Server/Routes/cardRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cardControllers = require('../Controllers/cardControllers');
+const authHelper = require('../utils/jwtHelpers');
 
 const router = express.Router();
 
 router.route('/all-cards').get(cardControllers.getAllCards);
-router.route('/game').get(cardControllers.getThreeCards);
+router.route('/game').get(authHelper.protect, cardControllers.getThreeCards);
 
 module.exports = router;
--- Server/utils/jwtHelpers.js
+++ Server/utils/jwtHelpers.js
@@ -1,11 +1,46 @@
 const jwt = require('jsonwebtoken');
 
+const { promisify } = require('util');
+const server = require('../db');
+
 // Create tokens for user function
-function createToken({id,username}) {
-  const user = {id,username};
-  const access_token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15d'}); 
- 
-  return({access_token});
+function createToken({ id, username }) {
+  const user = { id, username };
+  const access_token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '15d',
+  });
+
+  return { access_token };
 }
 
-module.exports = {createToken};
+const protect = async (req, res, next) => {
+  // 1) Getting token and check if it is there
+  let token;
+  if (req.cookies.access_token) {
+    token = req.cookies.access_token;
+  }
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided.' });
+  }
+
+  // 2) Verification of the token
+  const decoded = await promisify(jwt.verify)(
+    token,
+    process.env.ACCESS_TOKEN_SECRET
+  );
+
+  // 3) Check if user still exists
+  const pool = server.openDb();
+  const currentUser = await pool.query('SELECT * FROM users WHERE id = $1', [
+    decoded.id,
+  ]);
+  if (currentUser.rows.length === 0)
+    return res.status(404).json({ error: 'User no longer exists' });
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
+};
+
+module.exports = { createToken, protect };
