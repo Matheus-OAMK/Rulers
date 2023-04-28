69f090f8b320f923aa47169030f5329fabcd944e
ChpoCko <t2gaiu00@students.oamk.fi>
2023-04-28 23:20:48 +0000

    nOn apply in profile page labels go down

--- Scripts/account.js
+++ Scripts/account.js
@@ -1,48 +1,53 @@
-import server_back from './auth.js';
-import { requestOptions } from './helper.js';
+import server_back from "./auth.js";
+import { requestOptions } from "./helper.js";
 const server = new server_back();
-import { blueAlert, redAlert } from './alert.js';
+import { blueAlert, redAlert } from "./alert.js";
 // Get all .inputBox
-const inputBoxes = document.querySelectorAll('.form-input-box');
-const userName = document.querySelector('#data-user-name');
+const inputBoxes = document.querySelectorAll(".form-input-box");
+const userName = document.querySelector("#data-user-name");
 
 // For each box
-inputBoxes.forEach(box => {
+inputBoxes.forEach((box) => {
   // Get input as first child element
   const input = box.firstElementChild;
 
   // add event listener for any change on the input
-  input.addEventListener('input', () => {
+  input.addEventListener("input", () => {
     // if input is not empty add class filled
     // else remove class filled if is empty
     input.value
-      ? input.classList.add('filled')
-      : input.classList.remove('filled');
+      ? input.classList.add("filled")
+      : input.classList.remove("filled");
   });
 });
 
 // **********  PROFILE FUNCTION TO CHANGE PASSWORD**********
 
 function clearInputs() {
-  const passwordInput = document.querySelector('#currentpassword');
-  const newPasswordInput = document.querySelector('#newpassword1');
-  const confirmPasswordInput = document.querySelector('#newpassword2');
-  passwordInput.value = '';
-  newPasswordInput.value = '';
-  confirmPasswordInput.value = '';
+  const passwordInput = document.querySelector("#currentpassword");
+  const newPasswordInput = document.querySelector("#newpassword1");
+  const confirmPasswordInput = document.querySelector("#newpassword2");
+  passwordInput.value = "";
+  newPasswordInput.value = "";
+  confirmPasswordInput.value = "";
+
+  inputBoxes.forEach((box) => {
+    const input = box.firstElementChild;
+    input.classList.remove("filled");
+  });
 }
 
-const profileForm = document.querySelector('.settings-form1');
+const profileForm = document.querySelector(".settings-form1");
 
 const profileRoute = `${server.BACKEND_URL}/api/user/profile`;
 
-profileForm.addEventListener('submit', async event => {
+profileForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
-  const currentPassword = formData.get('password');
-  const newPassword = formData.get('newpassword1');
-  const confirmPassword = formData.get('newpassword2');
+  const currentPassword = formData.get("password");
+  const newPassword = formData.get("newpassword1");
+  const confirmPassword = formData.get("newpassword2");
   const data = { currentPassword, newPassword, confirmPassword };
 
   try {
@@ -65,7 +70,7 @@ profileForm.addEventListener('submit', async event => {
   }
 });
 
-server.checkAuth().then(res => {
+server.checkAuth().then((res) => {
   if (res.userData.username) {
     userName.textContent = res.userData.username;
   }
