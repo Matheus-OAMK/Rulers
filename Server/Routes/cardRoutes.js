1d01d35bec0fa4a5426d27f208c83f0b24bed6f1
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-04-28 03:13:02 +0300

    fixed router breaking because of parameter route being declared before static routes

--- Server/Routes/cardRoutes.js
+++ Server/Routes/cardRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route('/collection').get(authenticateToken, cardControllers.getUserCollection)
 router.route('/all-cards').get(cardControllers.getAllCards);
-router.route('/:id').post(authenticateToken, cardControllers.buyCard)
 router.route('/game').get(authenticateToken, cardControllers.getThreeCards).post(authenticateToken, cardControllers.insertCard)
+router.route('/:id').post(authenticateToken, cardControllers.buyCard)
 
 module.exports = router;
